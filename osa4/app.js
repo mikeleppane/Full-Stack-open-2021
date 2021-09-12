684179a8b9d1c7d5c1516d13e617bba64f445606
Mikko Leppänen <mleppan23@gmail.com>
2021-09-12 12:23:33 +0300

    Part4, exercises 4.1-4.2: Initial setup and code refactoring for the blog list project.

---
+++ osa4/app.js
@@ -0,0 +1,41 @@
+const config = require("./utils/config");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connection to MongoDB:", error.message);
+  });
+
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
+morgan.token("body", (req) => JSON.stringify(req.body));
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms - :body"
+  )
+);
+app.use(middleware.requestLogger);
+
+app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
